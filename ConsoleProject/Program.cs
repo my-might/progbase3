2a79d24864795d680091b9513b4e334b26e249a1
ValeriaKrivosheeva <valeriakrivosheeva333@gmail.com>
2021-04-25 23:39:27 +0300

    task2

--- ConsoleProject/Program.cs
+++ ConsoleProject/Program.cs
@@ -20,126 +20,20 @@ namespace ConsoleProject
                     string generatorPath = "/home/valeria/Desktop/progbase3/data/generator/";
                     Console.Write("Type of entity to generate: ");
                     string entity = Console.ReadLine();
-                    Console.Write("How many entities you want to generate? ");
-                    string numberOfEntities = Console.ReadLine();
-                    if(!int.TryParse(numberOfEntities, out int number) || number <= 0)
-                    {
-                        Console.WriteLine("Error: Number of entities must be positive integer.");
-                        continue;
-                    }
                     if(entity == "film")
                     {
                         FilmRepository repo = new FilmRepository(connection);
-                        Console.Write("Please, enter start of range of release years to generate: ");
-                        string inputStart = Console.ReadLine();
-                        if(!int.TryParse(inputStart, out int start) || start <= 0)
-                        {
-                            Console.WriteLine("Error: Year must be positive integer.");
-                            continue;
-                        }
-                        Console.Write("Please, enter end of range of release years to generate: ");
-                        string inputEnd = Console.ReadLine();
-                        if(!int.TryParse(inputEnd, out int end) || end <= 0 || end > 2021)
-                        {
-                            Console.WriteLine("Error: Year must be positive integer and not bigger than 2021.");
-                            continue;
-                        }
-                        if(start >= end)
-                        {
-                            Console.WriteLine("Error: start must be less than end.");
-                            continue;
-                        }
-                        Console.WriteLine("Data is generating...");
-                        for(int i = 0; i<number; i++)
-                        {
-                            string title = GenerateFromFile(generatorPath + "titles.txt");
-                            string genre = GenerateFromFile(generatorPath + "genres.txt");
-                            Random ran = new Random();
-                            int releaseYear = ran.Next(start, end+1);
-                            Film film = new Film();
-                            film.title = title;
-                            film.genre = genre;
-                            film.releaseYear = releaseYear;
-                            repo.Insert(film);
-                        }
-                        Console.WriteLine("Done!");
+                        ProcessGenerateFilms(generatorPath, repo);
                     }
                     else if(entity == "actor")
                     {
                         ActorRepository repo = new ActorRepository(connection);
-                        Console.Write("Please, enter start of range of birth dates to generate: ");
-                        string inputStart = Console.ReadLine();
-                        if(!DateTime.TryParse(inputStart, out DateTime start))
-                        {
-                            Console.WriteLine("Error: Birth date must be in date format.");
-                            continue;
-                        }
-                        Console.Write("Please, enter end of range of birth dates to generate: ");
-                        string inputEnd = Console.ReadLine();
-                        if(!DateTime.TryParse(inputEnd, out DateTime end))
-                        {
-                            Console.WriteLine("Error: Birth date must be in date format.");
-                            continue;
-                        }
-                        if(start >= end)
-                        {
-                            Console.WriteLine("Error: start must be less than end.");
-                            continue;
-                        }
-                        int range = (end-start).Days;
-                        Console.WriteLine("Data is generating...");
-                        for(int i = 0; i<number; i++)
-                        {
-                            string fullname = GenerateFromFile(generatorPath + "fullnames.txt");
-                            string country = GenerateFromFile(generatorPath + "countries.txt");
-                            Random ran = new Random();
-                            DateTime birthDate = start.AddDays(ran.Next(range));
-                            Actor actor = new Actor();
-                            actor.fullname = fullname;
-                            actor.country = country;
-                            actor.birthDate = birthDate;
-                            repo.Insert(actor);
-                        }
-                        Console.WriteLine("Done!");
+                        ProcessGenerateActors(generatorPath, repo);
                     }
                     else if(entity == "review")
                     {
                         ReviewRepository repo = new ReviewRepository(connection);
-                        Console.Write("Please, enter start of range of post datetime to generate: ");
-                        string inputStart = Console.ReadLine();
-                        if(!DateTime.TryParse(inputStart, out DateTime start))
-                        {
-                            Console.WriteLine("Error: Post datetime must be in date format.");
-                            continue;
-                        }
-                        Console.Write("Please, enter end of range of post datetime to generate: ");
-                        string inputEnd = Console.ReadLine();
-                        if(!DateTime.TryParse(inputEnd, out DateTime end))
-                        {
-                            Console.WriteLine("Error: Post datetime must be in date format.");
-                            continue;
-                        }
-                        if(start >= end)
-                        {
-                            Console.WriteLine("Error: start must be less than end.");
-                            continue;
-                        }
-                        TimeSpan range = end-start;
-                        Console.WriteLine("Data is generating...");
-                        for(int i = 0; i<number; i++)
-                        {
-                            string opinion = GenerateFromFile(generatorPath + "opinions.txt");
-                            Random ran = new Random();
-                            int rating = ran.Next(1, 11);
-                            TimeSpan ts = new TimeSpan((long)(ran.NextDouble() * range.Ticks));
-                            DateTime postedAt = start + ts;
-                            Review review = new Review();
-                            review.opinion = opinion;
-                            review.rating = rating;
-                            review.postedAt = postedAt;
-                            repo.Insert(review);
-                        }
-                        Console.WriteLine("Done!");
+                        ProcessGenerateReviews(generatorPath, repo);
                     }
                     else
                     {
@@ -154,7 +48,7 @@ namespace ConsoleProject
                 }
                 else
                 {
-                    Console.Error.WriteLine("Error: Unavailable command.");
+                    Console.Error.WriteLine("Error: Unavailable command. Try again.");
                     continue;
                 }
             }
@@ -173,6 +67,155 @@ namespace ConsoleProject
                 counter++;
             }
             return result;
+        }
+        static void ProcessGenerateFilms(string generatorPath, FilmRepository repo)
+        {
+            int number = GetNumberOfEntities();
+            int start = 0;
+            int end = 0;
+            while(true)
+            {
+                Console.Write("Please, enter start of range of release year to generate: ");
+                string inputStart = Console.ReadLine();
+                if(!int.TryParse(inputStart, out start) || start <= 0)
+                {
+                    Console.WriteLine("Error: Year must be positive integer. Try again.");
+                    continue;
+                }
+                Console.Write("Please, enter end of range of release year to generate: ");
+                string inputEnd = Console.ReadLine();
+                if(!int.TryParse(inputEnd, out end) || end <= 0 || end > 2021)
+                {
+                    Console.WriteLine("Error: Year must be positive integer and not bigger than 2021. Try again.");
+                    continue;
+                }
+                if(start >= end)
+                {
+                    Console.WriteLine("Error: Start must be less than end. Try again.");
+                    continue;
+                }
+                break;
+            }
+            Console.WriteLine("Data is generating...");
+            for(int i = 0; i<number; i++)
+            {
+                Film film = new Film();
+                film.title = GenerateFromFile(generatorPath + "titles.txt");
+                film.genre = GenerateFromFile(generatorPath + "genres.txt");
+                Random ran = new Random();
+                film.releaseYear = ran.Next(start, end+1);
+                repo.Insert(film);
+            }
+            Console.WriteLine("Done!");
+        }
+        static void ProcessGenerateActors(string generatorPath, ActorRepository repo)
+        {
+            int number = GetNumberOfEntities();
+            DateTime start;
+            DateTime end;
+            while(true)
+            {
+                Console.Write("Please, enter start of range of birth date to generate: ");
+                string inputStart = Console.ReadLine();
+                if(!DateTime.TryParse(inputStart, out start))
+                {
+                    Console.WriteLine("Error: Birth date must be in date format. Try again.");
+                    continue;
+                }
+                Console.Write("Please, enter end of range of birth date to generate: ");
+                string inputEnd = Console.ReadLine();
+                if(!DateTime.TryParse(inputEnd, out end))
+                {
+                    Console.WriteLine("Error: Birth date must be in date format. Try again.");
+                    continue;
+                }
+                if(end > DateTime.Now)
+                {
+                    Console.WriteLine("Error: End mustn`t be bigger than datetime now. Try again.");
+                    continue;
+                }
+                if(start >= end)
+                {
+                    Console.WriteLine("Error: Start must be less than end. Try again.");
+                    continue;
+                }
+                break;
+            }
+            int range = (end-start).Days;
+            Console.WriteLine("Data is generating...");
+            for(int i = 0; i<number; i++)
+            {
+                Actor actor = new Actor();
+                actor.fullname = GenerateFromFile(generatorPath + "fullnames.txt");
+                actor.country = GenerateFromFile(generatorPath + "countries.txt");
+                Random ran = new Random();
+                actor.birthDate = start.AddDays(ran.Next(range));
+                repo.Insert(actor);
+            }
+            Console.WriteLine("Done!");
+        }
+        static void ProcessGenerateReviews(string generatorPath, ReviewRepository repo)
+        {
+            int number = GetNumberOfEntities();
+            DateTime start;
+            DateTime end;
+            while(true)
+            {
+                Console.Write("Please, enter start of range of post datetime to generate: ");
+                string inputStart = Console.ReadLine();
+                if(!DateTime.TryParse(inputStart, out start))
+                {
+                    Console.WriteLine("Error: Post datetime must be in date format. Try again.");
+                    continue;
+                }
+                Console.Write("Please, enter end of range of post datetime to generate: ");
+                string inputEnd = Console.ReadLine();
+                if(!DateTime.TryParse(inputEnd, out end))
+                {
+                    Console.WriteLine("Error: Post datetime must be in date format. Try again.");
+                    continue;
+                }
+                if(end > DateTime.Now)
+                {
+                    Console.WriteLine("Error: End mustn`t be bigger than datetime now. Try again.");
+                    continue;
+                }
+                if(start >= end)
+                {
+                    Console.WriteLine("Error: Start must be less than end. Try again.");
+                    continue;
+                }
+                break;
+            }
+            TimeSpan range = end-start;
+            Console.WriteLine("Data is generating...");
+            for(int i = 0; i<number; i++)
+            {
+                Review review = new Review();
+                review.opinion = GenerateFromFile(generatorPath + "opinions.txt");
+                Random ran = new Random();
+                review.rating = ran.Next(1, 11);
+                TimeSpan ts = new TimeSpan((long)(ran.NextDouble() * range.Ticks));
+                review.postedAt = start + ts;
+                repo.Insert(review);
+            }
+            Console.WriteLine("Done!");
+        }
+        static int GetNumberOfEntities()
+        {
+            int number = 0;
+            while(true)
+            {
+                Console.Write("How many entities you want to generate? ");
+                string numberOfEntities = Console.ReadLine();
+                if(!int.TryParse(numberOfEntities, out number) || number <= 0)
+                {
+                    Console.WriteLine("Error: Number of entities must be positive integer. Try again.");
+                    continue;
+                }
+                break;
+            }
+            return number;
         }
     }
 }
